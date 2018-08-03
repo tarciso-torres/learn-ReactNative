070f3c185e80fbfa97583feff406bbf16de57d2b
Tarciso Torres <tarciso.torres@ifactory.com.br>
2018-08-03 08:52:38 -0300

    Open url in browser

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button'
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album
+    const { title, artist, thumbnail_image, image, url } = album
     const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles
     return(
         <Card>
@@ -26,7 +26,9 @@ const AlbumDetail = ({ album }) => {
                  source={{ uri: image }}/>
             </CardSection>
             <CardSection>
-                <Button onPress={() => console.log(title)} />
+                <Button onPress={() => Linking.openURL(url)} >
+                    Buy now!!
+                </Button>
             </CardSection>
         </Card>
     ); 
